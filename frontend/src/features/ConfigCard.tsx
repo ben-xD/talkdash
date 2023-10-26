3843acef82ec89c40da83438d0f9022ea83923fb
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-10-27 00:31:11 +0100

    feat(frontend): support undo and redo

--- frontend/src/features/ConfigCard.tsx
+++ frontend/src/features/ConfigCard.tsx
@@ -1,19 +1,30 @@
-import { createSignal } from "solid-js";
-import { finishTime, setFinishTime, setStartTime } from "./time/timeState.ts";
+import {
+  finishTime,
+  redo,
+  redoStack,
+  setTextInputDurationInMinutes,
+  setTimeAction,
+  textInputDurationInMinutes,
+  undo,
+  undoStack,
+} from "./time/timeState.ts";
 import { DateTime } from "luxon";
+import { RightIcon } from "../assets/RightIcon.tsx";
+import { LeftIcon } from "../assets/LeftIcon.tsx";
 
 // {/*TODO handle fuzzy input (e.g. 10 mins, 20 minutes, 1hr20m, average lifetime of an owl) */}
 export function ConfigCard() {
-  const [durationInMinutes, setDurationInMinutes] = createSignal("");
-
   const onStart = () => {
     const startTime = DateTime.now();
 
-    const minutes = parseFloat(durationInMinutes());
     // TODO handle parseFloat error.
+    const minutes = parseFloat(textInputDurationInMinutes());
     const finishTime = startTime.plus({ minutes });
-    setStartTime(startTime);
-    setFinishTime(finishTime);
+    setTimeAction({
+      finishTime,
+      startTime,
+      userTalkLengthInput: textInputDurationInMinutes(),
+    });
   };
 
   return (
@@ -37,27 +48,49 @@ export function ConfigCard() {
           class="bg-blue-200 rounded-lg p-2 w-full"
           type="text"
           id="finishTime"
-          onInput={(e) => setDurationInMinutes(e.target.value)}
+          value={textInputDurationInMinutes()}
+          onInput={(e) => setTextInputDurationInMinutes(e.target.value)}
         />
       </div>
-      <div class="flex flex-wrap gap-2 text-blue-50 w-full justify-end">
-        <button
-          disabled={!!finishTime() || !durationInMinutes()}
-          class="bg-green-600 px-4 py-2 rounded-md disabled:bg-gray-500"
-          onClick={onStart}
-        >
-          Start
-        </button>
-        <button
-          disabled={!finishTime()}
-          class="bg-red-600 disabled:bg-gray-500 px-4 py-2 rounded-md"
-          onClick={() => {
-            setStartTime(undefined);
-            setFinishTime(undefined);
-          }}
-        >
-          Reset
-        </button>
+      <div class="flex flex-wrap gap-2 text-blue-50 w-full justify-between">
+        <div class="flex gap-2">
+          <button
+            class="px-4 py-2 rounded-md disabled:bg-gray-400 bg-gray-900"
+            onClick={() => undo()}
+            disabled={undoStack.length === 0}
+          >
+            <LeftIcon />
+          </button>
+          <button
+            class="px-4 py-2 rounded-md disabled:bg-gray-400 bg-gray-900"
+            onClick={() => redo()}
+            disabled={redoStack.length === 0}
+          >
+            <RightIcon />
+          </button>
+        </div>
+        <div class="flex gap-2">
+          <button
+            disabled={!!finishTime() || !textInputDurationInMinutes()}
+            class="bg-green-600 px-4 py-2 rounded-md disabled:bg-gray-400"
+            onClick={onStart}
+          >
+            Start
+          </button>
+          <button
+            disabled={!finishTime()}
+            class="bg-red-600 disabled:bg-gray-400 px-4 py-2 rounded-md"
+            onClick={() => {
+              setTimeAction({
+                finishTime: undefined,
+                startTime: undefined,
+                userTalkLengthInput: textInputDurationInMinutes(),
+              });
+            }}
+          >
+            Reset
+          </button>
+        </div>
       </div>
     </div>
   );
--- frontend/src/features/time/timeState.ts
+++ frontend/src/features/time/timeState.ts
@@ -1,5 +1,9 @@
 import { createSignal } from "solid-js";
 import { DateTime } from "luxon";
+import { createStore } from "solid-js/store";
+
+export const [textInputDurationInMinutes, setTextInputDurationInMinutes] =
+  createSignal("");
 
 export const [finishTime, setFinishTime] = createSignal<DateTime | undefined>(
   undefined,
@@ -10,6 +14,68 @@ export const [startTime, setStartTime] = createSignal<DateTime | undefined>(
 export const [currentTime, setCurrentTime] = createSignal<DateTime>(
   DateTime.now(),
 );
+
+export type TimeAction =
+  | {
+      userTalkLengthInput: string;
+      startTime: DateTime;
+      finishTime: DateTime;
+    }
+  | {
+      userTalkLengthInput: string;
+      startTime: undefined;
+      finishTime: undefined;
+    };
+
+export const [undoStack, setUndoStack] = createStore<TimeAction[]>([]);
+export const [redoStack, setRedoStack] = createStore<TimeAction[]>([]);
+
+export const setTimeAction = (
+  action: TimeAction,
+  clearRedoStack: boolean = true,
+) => {
+  // add to undo stack whenever a change is made so we can undo it.
+  setUndoStack([...undoStack, action]);
+  setFinishTime(action.finishTime);
+  setStartTime(action.startTime);
+
+  if (clearRedoStack) {
+    // The user performs an action after undoing, so we should clear the
+    // redo stack so they can't redo something after their new action.
+    setRedoStack([]);
+  }
+};
+
+export const undo = () => {
+  // Pop the last undo and put it in redo.
+  // Play the last action after that.
+  const actionToUndo = undoStack.slice(-1).at(0);
+  setUndoStack([...undoStack.slice(0, -1)]);
+  const lastAction = undoStack.at(undoStack.length - 1);
+  if (actionToUndo) {
+    setUndoStack(undoStack);
+    setRedoStack([...redoStack, actionToUndo]);
+    if (lastAction) {
+      setTextInputDurationInMinutes(lastAction.userTalkLengthInput);
+      setFinishTime(lastAction.finishTime);
+      setStartTime(lastAction.startTime);
+    } else {
+      setFinishTime(undefined);
+      setStartTime(undefined);
+    }
+  }
+};
+
+export const redo = () => {
+  // pop from redo stack.
+  const actionToRedo = redoStack.slice(-1).at(0);
+  setRedoStack([...redoStack.slice(0, -1)]);
+  if (actionToRedo) {
+    setTextInputDurationInMinutes(actionToRedo.userTalkLengthInput);
+    setRedoStack([...redoStack]);
+    setTimeAction(actionToRedo, false);
+  }
+};
 
 export const difference = (start: DateTime, finish: DateTime) => {
   const isExceeded = finish < start;
