8713307bc4faedb15e2aeeca0fcabb3c6cf6a597
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-10-28 11:32:50 +0100

    feat(frontend): make frontend PWA so users can use it offline
    This adds a service worker

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,9 +1,50 @@
 import { defineConfig } from "vite";
 import solid from "vite-plugin-solid";
+import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
   // Prevent clearing screen so that there are no glitches in the output of `turbo dev`
-  // when both the frontend and backend are started..
+  // when both the frontend and backend are started.
   clearScreen: false,
-  plugins: [solid()],
+  plugins: [
+    solid(),
+    VitePWA({
+      registerType: "autoUpdate",
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,ico,png,svg,ttf,webmanifest}"],
+      },
+      devOptions: {
+        enabled: true,
+      },
+      manifest: {
+        short_name: "TalkDash",
+        name: "TalkDash",
+        description: "Remote stopwatch with messaging functionality.",
+        id: "uk.orth.talkdash",
+        start_url: "/",
+        background_color: "#1d4ed8",
+        display: "standalone",
+        scope: "/",
+        theme_color: "#1d4ed8",
+        icons: [
+          {
+            src: "/images/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/images/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "/images/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
