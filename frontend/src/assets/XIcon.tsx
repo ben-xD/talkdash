1d38cf2201583f37cff946060da35b849774a36f
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-11-07 09:43:56 +0000

    refactor(frontend): make more icons class-overridable

--- frontend/src/assets/XIcon.tsx
+++ frontend/src/assets/XIcon.tsx
@@ -1,11 +1,13 @@
-export const XIcon = () => (
+import { cn } from "../css/tailwind.ts";
+
+export const XIcon = (props: { class: string }) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     fill="none"
     viewBox="0 0 24 24"
     stroke-width="1.5"
     stroke="currentColor"
-    class="h-6 w-6"
+    class={cn("h-6 w-6", props.class)}
   >
     <path
       stroke-linecap="round"
