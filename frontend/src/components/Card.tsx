a9073fbf1cb91727640811c52e93dabcbe720ee9
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-11-12 21:42:02 +0000

    fix(frontend): card colors

--- frontend/src/components/Card.tsx
+++ frontend/src/components/Card.tsx
@@ -4,8 +4,13 @@ import { cn } from "../css/tailwind";
 
 export const Card = (props: { children?: JSX.Element; class?: ClassValue }) => {
   return (
-    <div class={cn("z-10 h-full rounded-xl bg-blue-50 shadow-lg", props.class)}>
-      <div class="flex h-full flex-col items-stretch justify-between gap-8 text-blue-800 dark:bg-blue-900 dark:text-blue-200">
+    <div
+      class={cn(
+        "z-10 h-full rounded-xl bg-blue-50 text-blue-800 shadow-lg dark:bg-blue-900 dark:text-blue-200",
+        props.class,
+      )}
+    >
+      <div class="flex h-full flex-col items-stretch justify-between gap-8">
         {props.children}
       </div>
     </div>
