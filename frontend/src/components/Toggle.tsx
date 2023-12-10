b9cd46d8986544154d4b1cd5305d8ff47367a584
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-12-10 15:10:25 +0000

    refactor(frontend): toggle component

---
+++ frontend/src/components/Toggle.tsx
@@ -0,0 +1,37 @@
+import { cn } from "../css/tailwind.ts";
+
+export const Toggle = (props: {
+  checked: boolean;
+  setChecked: (checked: boolean) => void;
+  "aria-label"?: string;
+}) => {
+  return (
+    <button
+      class={cn(
+        "relative inline-flex h-6 w-11 cursor-pointer rounded-full border-[2px] border-transparent bg-opacity-100 transition-all duration-200 ease-in-out focus:ring focus:ring-primary-500",
+        {
+          "bg-primary-500": props.checked,
+          "bg-primary-800": !props.checked,
+        },
+      )}
+      role="switch"
+      type="button"
+      aria-checked={props.checked}
+      onClick={() => props.setChecked(!props.checked)}
+    >
+      <span class="absolute m-[-1px] h-[1px] w-[1px] shrink-0 overflow-hidden whitespace-nowrap border-0 p-0">
+        {props["aria-label"]}
+      </span>
+      <span
+        aria-hidden="true"
+        class={cn(
+          "h-5 w-5 rounded-full bg-primary-50 transition-all duration-200 ease-in-out",
+          {
+            "translate-x-0": !props.checked,
+            "translate-x-5": props.checked,
+          },
+        )}
+      ></span>
+    </button>
+  );
+};
--- frontend/src/features/speaker/MetadataView.tsx
+++ frontend/src/features/speaker/MetadataView.tsx
@@ -10,6 +10,7 @@ import {
   isQrCodeShown,
   setIsQrCodeShown,
 } from "../../components/QrCodeView.tsx";
+import { Toggle } from "../../components/Toggle.tsx";
 
 type Props = {
   reconnectAsSpeaker: (speakerUsername: string) => void;
@@ -61,36 +62,11 @@ export const MetadataView = (props: Props) => {
       />
       <div class="flex justify-between">
         <p>Show QR code</p>
-        <button
-          class={cn(
-            "relative inline-flex h-6 w-11 cursor-pointer rounded-full border-[2px] border-transparent bg-opacity-100 transition-all duration-200 ease-in-out focus:ring focus:ring-primary-500",
-            { "bg-primary-500": isQrCodeShown() },
-          )}
-          role="switch"
-          type="button"
-          aria-checked={isQrCodeShown()}
-          onClick={() => setIsQrCodeShown(!isQrCodeShown())}
-        >
-          <span class="absolute m-[-1px] h-[1px] w-[1px] shrink-0 overflow-hidden whitespace-nowrap border-0 p-0">
-            Toggle QR code
-          </span>
-          <span
-            aria-hidden="true"
-            class={cn(
-              "h-5 w-5 rounded-full bg-primary-50 transition-all duration-200 ease-in-out",
-              {
-                "translate-x-0": !isQrCodeShown(),
-                "translate-x-5": isQrCodeShown(),
-              },
-            )}
-          ></span>
-        </button>
-        {/*<input*/}
-        {/*  type="checkbox"*/}
-        {/*  class="toggle border-primary-500 bg-primary-500 hover:bg-primary-300"*/}
-        {/*  checked={isQrCodeShown()}*/}
-        {/*  onChange={(e) => setIsQrCodeShown(e.target.checked)}*/}
-        {/*/>*/}
+        <Toggle
+          aria-label={"Toggle QR code"}
+          checked={isQrCodeShown()}
+          setChecked={setIsQrCodeShown}
+        />
       </div>
       {/*Temporarily disable password field since it is not implemented.*/}
       {/*<EditableStateField*/}
