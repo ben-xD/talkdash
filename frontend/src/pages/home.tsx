21f2aa3edd3020ae21cd294455713fecdaca4dbf
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-12-10 11:41:37 +0000

    feat(frontend): simplify home page

--- frontend/src/pages/home.tsx
+++ frontend/src/pages/home.tsx
@@ -14,34 +14,34 @@ const Home = () => {
   return (
     <div class="my-4 flex max-w-[400px] flex-col gap-12">
       {isSignedIn() ? (
-        <p class="text-xl">Welcome 👋, you're logged in</p>
+        <p class="text-center text-xl">Welcome 👋, you're logged in</p>
       ) : (
-        <p class="text-xl">Hey 👋</p>
+        <></>
       )}
-      <p>
-        This is a timer for events. Speakers see a{" "}
-        <span class="font-bold">large timer</span>. Audience and hosts can send
-        them messages.
+      <p class="text-center">
+        Keeping speakers on time and sending them live messages during talks
       </p>
-      <div class="flex flex-col gap-8 text-sm">
-        <p>What's your role?</p>
+      <div class="flex flex-col gap-8">
+        <p class="text-center">
+          What's your <span class="font-bold">role?</span>
+        </p>
         <div class="flex flex-wrap justify-between gap-4 text-center">
           <A
-            class="flex w-24 shrink-0 grow flex-col items-center justify-center gap-2 rounded-lg p-4 outline outline-primary-100 hover:text-primary-200"
+            class="flex w-24 shrink-0 grow flex-col items-center justify-center gap-2 rounded-lg p-4 outline outline-primary-50 hover:text-primary-200"
             href="/audience"
           >
             <UsersIcon />
             <p>Audience</p>
           </A>
           <A
-            class="flex w-24 shrink-0 grow flex-col items-center justify-center gap-2 rounded-lg p-4 outline outline-primary-100 hover:text-primary-200"
+            class="flex w-24 shrink-0 grow flex-col items-center justify-center gap-2 rounded-lg p-4 outline outline-primary-50 hover:text-primary-200"
             href="/speaker"
           >
             <MicIcon />
             <p>Speaker</p>
           </A>
           <A
-            class="flex w-24 shrink-0 grow flex-col items-center justify-center gap-2 rounded-lg p-4 outline outline-primary-100 hover:text-primary-200"
+            class="flex w-24 shrink-0 grow flex-col items-center justify-center gap-2 rounded-lg p-4 outline outline-primary-50 hover:text-primary-200"
             href="/host"
           >
             <ConfettiIcon />
@@ -49,20 +49,18 @@ const Home = () => {
           </A>
         </div>
       </div>
-      <div class="flex flex-col gap-4">
+      <div class="flex flex-col gap-4 text-center">
         <p>
           By{" "}
           <a class="font-bold underline" href="https://orth.uk/">
             Ben Butterworth
           </a>
         </p>
-        <a href="https://orth.uk/">
-          <img
-            src={TalkDashImage}
-            alt={"Image of a blue bird called TalkDash."}
-            class="w-40 drop-shadow-xl dark:brightness-90"
-          />
-        </a>
+        <img
+          src={TalkDashImage}
+          alt={"Image of a blue bird called TalkDash."}
+          class="m-auto block w-40 drop-shadow-xl dark:brightness-90 "
+        />
       </div>
     </div>
   );
