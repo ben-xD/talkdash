26bd1404af7d7afbeb0084da167735fda4f762bc
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-12-16 11:20:37 +0000

    refactor(backend): oauth data handling

--- backend/src/auth/oauth.ts
+++ backend/src/auth/oauth.ts
@@ -1,34 +1,43 @@
 import { uuid } from "../uuid.js";
-import { GithubUser, GoogleUser } from "./auth.js";
+import { Auth } from "./auth.js";
+import { GithubUserAuth, GoogleUserAuth } from "@lucia-auth/oauth/providers";
 
 // Consider supporting multiple OAuth providers linked to the same user
 // See https://lucia-auth.com/oauth/basics/handle-users/#add-a-new-key-to-an-existing-user
 
-export const getOrCreateUserFromGoogleUser = async (googleUser: GoogleUser) => {
-  const existingUser = await googleUser.getExistingUser();
+export const getOrCreateUserFromGoogleUser = async (
+  auth: GoogleUserAuth<Auth>,
+) => {
+  const existingUser = await auth.getExistingUser();
   if (existingUser) return existingUser;
-  return await googleUser.createUser({
+
+  const { email, name } = auth.googleUser;
+  return await auth.createUser({
     userId: uuid(),
     attributes: {
-      email: googleUser.googleUser.email ?? null,
+      email: email ?? null,
       created_at: new Date(),
       updated_at: new Date(),
-      name: null,
+      name: name,
       username: null,
     },
   });
 };
 
-export const getOrCreateUserFromGithubUser = async (githubUser: GithubUser) => {
-  const existingUser = await githubUser.getExistingUser();
+export const getOrCreateUserFromGithubUser = async (
+  auth: GithubUserAuth<Auth>,
+) => {
+  const existingUser = await auth.getExistingUser();
   if (existingUser) return existingUser;
-  return await githubUser.createUser({
+
+  const { email, name } = auth.githubUser;
+  return await auth.createUser({
     userId: uuid(),
     attributes: {
-      email: githubUser.githubUser.email ?? null,
+      email: email ?? null,
       created_at: new Date(),
       updated_at: new Date(),
-      name: null,
+      name: name ?? null,
       username: null,
     },
   });
