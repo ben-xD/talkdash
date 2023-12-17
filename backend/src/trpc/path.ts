b8453ba26d685695f415fc438bf69211779d6754
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-12-17 20:10:13 +0000

    refactor: remove unused code

--- backend/src/trpc/path.ts
+++ backend/src/trpc/path.ts
@@ -1,8 +1,3 @@
 export const trpcHttpApiPath = "/trpc/api/v1";
 export const trpcWebsocketApiPath = "/trpc/api/v1/ws";
 export const trpcPanelPath = "/trpc";
-
-// Authentication
-export const httpApiPath = "/http";
-const authApiPath = `${httpApiPath}/auth`;
-export const githubAuthCallbackPath = `${authApiPath}/callback/github`;
