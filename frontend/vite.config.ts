1eedd3223c0c13096b65f0f8bdb9a707c8e3dee5
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-12-17 01:07:23 +0000

    chore: configure CI to use sentry auth token

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -4,6 +4,16 @@ import solid from "vite-plugin-solid";
 import solidDevtools from "solid-devtools/vite";
 import { VitePWA } from "vite-plugin-pwa";
 
+// This is only available if the environment variable is set, not if it is in the .env file.
+// The auth token is available there, just `export` it to test publishing sourcemaps to sentry.
+const sentryAuthToken = process.env.SENTRY_AUTH_TOKEN;
+const isRunningOnCloudflareCi = process.env.CF_PAGES_BRANCH;
+if (isRunningOnCloudflareCi && !sentryAuthToken) {
+  throw new Error(
+    "Sentry auth token not set, cannot publish sourcemaps to sentry.",
+  );
+}
+
 export default defineConfig({
   // Prevent clearing screen so that there are no glitches in the output of `turbo dev`
   // when both the frontend and backend are started.
@@ -56,6 +66,7 @@ export default defineConfig({
     sentryVitePlugin({
       org: "orthuk",
       project: "talkdash",
+      authToken: sentryAuthToken,
     }),
   ],
 
