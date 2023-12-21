cdf9ecb061c0496149a3c285b82100a0b25974d7
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-12-21 00:30:36 +0000

    docs: replace staging database with supabase

--- backend/setupTests/vitest-env.d.ts
+++ backend/setupTests/vitest-env.d.ts
@@ -3,6 +3,7 @@ import * as pg from "pg";
 // We need to import this file (a module augmentation - declaration merging)
 declare module "vitest" {
   // For sharing data in the same test (not for sharing between tests)
+  // docs: https://vitest.dev/guide/test-context.html#typescript-1
   export interface TestContext {
     dbPool?: pg.Pool;
   }
