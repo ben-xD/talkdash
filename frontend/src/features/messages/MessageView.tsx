9c009151ea64b089fe98598d456f55bdf1cde336
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-12-24 13:35:14 +0000

    feat(frontend): show hidden messages count when some messages are hidden

--- frontend/src/features/messages/MessageView.tsx
+++ frontend/src/features/messages/MessageView.tsx
@@ -1,14 +1,30 @@
 import { createSignal, For, onCleanup, Show } from "solid-js";
-import { receivedMessages, setReceivedMessages } from "./receivedMessages";
+import {
+  ReceivedMessage,
+  receivedMessages,
+  setReceivedMessages,
+} from "./receivedMessages";
 import { TrashIcon } from "../../assets/TrashIcon";
 import { isAudienceMessagesShown } from "../speaker/audienceMessages.ts";
 
 export const MessageView = () => {
+  const showMessage = (message: ReceivedMessage) => {
+    if (message.sender.role === "audience") {
+      return isAudienceMessagesShown();
+    }
+    return true;
+  };
+
+  const shownMessages = () => receivedMessages.filter(showMessage);
+  const hiddenMessageCount = () =>
+    receivedMessages.length - shownMessages().length;
+
   return (
     <div class="flex w-full flex-col items-center">
       <div class="flex items-center gap-8">
         <h2 class="font-bold tracking-tight">
-          Messages ({receivedMessages.length})
+          Messages ({shownMessages().length}
+          {hiddenMessageCount() > 0 && `, ${hiddenMessageCount()} hidden`})
         </h2>
         <TrashIcon
           class="h-12 w-12 p-3 hover:text-primary-100 active:text-white"
@@ -16,7 +32,7 @@ export const MessageView = () => {
         />
       </div>
       <div class="flex w-full flex-col gap-4">
-        <For each={receivedMessages}>
+        <For each={shownMessages()}>
           {(message) => {
             const [timeSinceReceived, setTimeSinceReceived] = createSignal(
               message.receivedAt.toRelative(),
@@ -29,11 +45,8 @@ export const MessageView = () => {
               clearInterval(interval);
             });
 
-            const hideMessage = () =>
-              message.sender.role === "audience" && !isAudienceMessagesShown();
-
             return (
-              <Show when={timeSinceReceived() && !hideMessage()}>
+              <Show when={timeSinceReceived()}>
                 <div class="flex w-full animate-jackInTheBox items-center justify-between gap-8 @container">
                   <p class="whitespace-pre-wrap @lg:text-[4cqw]">
                     {message.emojiMessage
