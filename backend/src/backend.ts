95a344b749f94a8c516341d1185227111e9c1c41
Ben Butterworth <24711048+ben-xD@users.noreply.github.com>
2023-12-24 16:04:28 +0000

    refactor(backend): remove unnecessary workaround for top level await

--- backend/src/backend.ts
+++ backend/src/backend.ts
@@ -24,28 +24,25 @@ const auth = createAuth(dbPool);
 const oAuths = createOAuths(auth);
 registerTrpcApis(fastify, db, auth, oAuths);
 
-(async () => {
-  try {
-    const address = env.INSIDE_DOCKER ? "0.0.0.0" : "127.0.0.1";
-    const port = env.PORT ? parseInt(env.PORT, 10) : 4000;
-    await fastify.listen({ port, host: address });
-    console.info(
-      chalk.green(
-        `tRPC HTTP API on http://localhost:${port}${trpcHttpApiPath}`,
-      ),
-    );
-    console.info(
-      chalk.green(
-        `tRPC WebSocket API on http://localhost:${port}${trpcWebsocketApiPath}`,
-      ),
-    );
-    console.info(
-      chalk.green(
-        `API UI (tRPC Panel) on http://localhost:${port}${trpcPanelPath}`,
-      ),
-    );
-  } catch (err) {
-    fastify.log.error(err);
-    process.exit(1);
-  }
-})();
+const address = env.INSIDE_DOCKER ? "0.0.0.0" : "127.0.0.1";
+const port = env.PORT ? parseInt(env.PORT, 10) : 4000;
+
+try {
+  await fastify.listen({ port, host: address });
+  console.info(
+    chalk.green(`tRPC HTTP API on http://${address}:${port}${trpcHttpApiPath}`),
+  );
+  console.info(
+    chalk.green(
+      `tRPC WebSocket API on http://${address}:${port}${trpcWebsocketApiPath}`,
+    ),
+  );
+  console.info(
+    chalk.green(
+      `API UI (tRPC Panel) on http://${address}:${port}${trpcPanelPath}`,
+    ),
+  );
+} catch (err) {
+  fastify.log.error(err);
+  process.exit(1);
+}
